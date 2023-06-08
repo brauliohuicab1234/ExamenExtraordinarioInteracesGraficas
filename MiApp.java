cbebc9d2ee3d3d1b9ee307c90f52085174bbc37e
brauliohuicab1234 <129221141+brauliohuicab1234@users.noreply.github.com>
2023-06-07 21:51:53 -0600

    Add files via upload

---
+++ MiApp.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mi.app;
+
+/**
+ *
+ * @author PC
+ */
+public class MiApp {
+        public static void main(String[] args) {
+        String nombre = "BraulioRafaelHuicabMay";
+        String matricula = "63913";
+        String correo = "al063913@uacam.mx";
+        
+        // Imprime los datos en pantalla
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Correo electrónico: " + correo);
+        
+        // Guarda los datos en algún lugar si es necesario
+        // ...
+        
+    }
+}
